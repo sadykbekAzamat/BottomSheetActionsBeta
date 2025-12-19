2530f1c3165fa4eeb0ffccf4b17eecf961cc75c0
Azamat Sadykbek <31741@iitu.edu.kz>
2025-12-19 17:30:56 +0500

    fix main activity

--- app/src/main/java/kz/beta/bottomsheetactionsbeta/MainActivity.java
+++ app/src/main/java/kz/beta/bottomsheetactionsbeta/MainActivity.java
@@ -1,10 +1,6 @@
 package kz.beta.bottomsheetactionsbeta;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
-import kz.beta.bottom_sheet_beta.BottomSheetBeta;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,38 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        List<BottomSheetBeta.ActionItem> actions = new ArrayList<>();
-
-        actions.add(new BottomSheetBeta.ActionItem(
-                "WhatsApp",
-                () -> Toast.makeText(MainActivity.this, "WhatsApp", Toast.LENGTH_SHORT).show(),
-                true
-        ));
-
-        actions.add(new BottomSheetBeta.ActionItem(
-                "Позвонить",
-                () -> Toast.makeText(MainActivity.this, "Позвонить", Toast.LENGTH_SHORT).show(),
-                true
-        ));
-
-        actions.add(new BottomSheetBeta.ActionItem(
-                "Написать в чат",
-                () -> Toast.makeText(MainActivity.this, "Написать в чат", Toast.LENGTH_SHORT).show(),
-                false
-        ));
-
-        BottomSheetBeta sheet =
-                BottomSheetBeta.newInstance(
-                        actions,
-                        60,
-                        "Выберите",
-                        () -> Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show(),
-                        "Жабу"
-                );
-
-        sheet.show(getSupportFragmentManager(), "contact_sheet");
-
 
     }
 }
